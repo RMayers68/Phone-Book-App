c5b3c6bb2a6b7a5d47d378348861ee4a3970b4a5
Richard Mayers <rmayers68@gmail.com>
2022-10-18 14:01:03 -0700

    Add project files.

---
+++ Phone Book App/Contact.cs
@@ -0,0 +1,16 @@
+﻿namespace Phone_Book_App
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public Contact(int id, string name, string phoneNumber)
+        {
+            Id = id;
+            Name = name;
+            PhoneNumber = phoneNumber;
+        }   
+    }
+}
