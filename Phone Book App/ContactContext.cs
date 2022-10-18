1659e75baa603f4a1254012f5f7eb7a9ede223c7
Richard Mayers <rmayers68@gmail.com>
2022-10-18 16:08:23 -0700

    Create main menu using ConsoleTableExt

--- Phone Book App/ContactContext.cs
+++ Phone Book App/ContactContext.cs
@@ -1,21 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System.Configuration;
-using System.Collections.Specialized;
-using System.Web;
-using System.Xml.Linq;
-
 
 namespace Phone_Book_App
 {
     public class ContactContext : DbContext
     {
         public DbSet<Contact> Contacts { get; set; }
+        public string DbPath = $"server=localhost;Database=Phonebook;Trusted_Connection=True;";
 
         // The following configures EF to create a Sql Server database file in the
         // special "local" folder for your platform.
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
-            options.UseSqlServer($"server=localhost;Database=Phonebook;Trusted_Connection=True;");
+            options.UseSqlServer(DbPath);
         }
     }
 }
--- Phone Book App/Program.cs
+++ Phone Book App/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace Phone_Book_App
+﻿using ConsoleTableExt;
+
+namespace Phone_Book_App
 {
     class Program
     {
         static void Main(string[] args)
         {
+            bool closeProgram = false;
+            string[] menu = { "Phone Book", "1: Add a Phone #", "2: Delete a Phone #", "3: Update an existing Phone #", "4: Delete a Phone #" };
+            List<string> menuList = new(menu);
+            while (!closeProgram)
+            {               
+                Console.Clear();
+                ConsoleTableBuilder.From(menuList).ExportAndWriteLine(TableAligntment.Center);
+                Console.ReadKey();
+            }   
         }
     }
 
