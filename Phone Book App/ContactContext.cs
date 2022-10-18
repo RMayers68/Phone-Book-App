ed43f3b521e843ed8828b231043e1e9a5601935b
Richard Mayers <rmayers68@gmail.com>
2022-10-18 14:38:14 -0700

    Download neccessary packages and create Context class

---
+++ Phone Book App/ContactContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace Phone_Book_App
+{
+    public class ContactContext : DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+        public string DbPath { get; }
+
+        public ContactContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "contacts.db");
+        }
+
+        // The following configures EF to create a Sql Server database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer($"Data Source={DbPath}");
+        }
+    }
+}
